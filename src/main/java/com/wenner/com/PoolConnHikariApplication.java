2ba2a12244fdb44b31f661b6bb2a2b89def7e9f3
wenner-sanner <wennersanner@hotmail.com>
2018-03-18 22:00:23 -0300

    Primeiro commit

---
+++ src/main/java/com/wenner/com/PoolConnHikariApplication.java
@@ -0,0 +1,12 @@
+package com.wenner.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolConnHikariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolConnHikariApplication.class, args);
+	}
+}
